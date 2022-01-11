beef838f514a37fa97936e4d9c9f718b69d95ce5
Romain d'Alverny <rdalverny@gmail.com>
2022-01-11 18:56:33 +0100

    Adjust Atom feed dates timezone, entry.id

--- atom.php
+++ atom.php
@@ -20,22 +20,20 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
     <id><?=$PlanetConfig->getUrl()?></id>
     <link rel="self" type="application/atom+xml" href="<?=$PlanetConfig->getUrl()?>atom.php" />
     <link rel="alternate" type="text/html" href="<?=$PlanetConfig->getUrl()?>" />
-    <updated><?=date("Y-m-d\TH:i:s\Z")?></updated>
+    <updated><?=gmdate('Y-m-d\TH:i:s\Z')?></updated>
     <author><name><?=htmlspecialchars($PlanetConfig->getName())?></name></author>
-
 <?php $count = 0; ?>
 <?php foreach ($items as $item) : ?>
+
     <entry>
         <title type="html"><?=htmlspecialchars($item->get_feed()->getName())?> : <?=htmlspecialchars($item->get_title())?></title>
-        <id><?=htmlspecialchars($item->get_permalink())?></id>
+        <id><?=htmlspecialchars($item->get_id())?></id>
         <link rel="alternate" href="<?=htmlspecialchars($item->get_permalink())?>"/>
-        <published><?=$item->get_date('Y-m-d\\TH:i:s+00:00')?></published>
-        <updated><?=$item->get_date('Y-m-d\\TH:i:s+00:00')?></updated>
+        <published><?=$item->get_gmdate('Y-m-d\TH:i:s\Z')?></published>
+        <updated><?=$item->get_gmdate('Y-m-d\TH:i:s\Z')?></updated>
         <author><name><?=($item->get_author() ? $item->get_author()->get_name() : 'anonymous')?></name></author>
-
         <content type="html"><![CDATA[<?=$item->get_content()?>]]></content>
     </entry>
-
     <?php if (++$count == $limit) {
         break;
     } ?>
